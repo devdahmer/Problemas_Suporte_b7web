310a7760864a780c87c84e39196841f3bc2c1c73
Eduardo Dahmer Correa <69122329+devdahmer@users.noreply.github.com>
2020-11-11 13:46:55 -0300

    Update UsuarioDaoMysql.php
    Update de Código

--- DAO/dao/UsuarioDaoMysql.php
+++ DAO/dao/UsuarioDaoMysql.php
@@ -12,21 +12,18 @@ class UsuarioDaoMysql implements UsuarioDAO {
 
     }
     public function findAll() {
-    $array = [];
-
+        $array = [];
         $sql = $this->pdo->query("SELECT * FROM usuarios");
         if($sql->rowCount() > 0) {
             $data = $sql->fetchAll();
-
             foreach($data as $item) {
                 $u = new Usuario();
                 $u->setId($item['id']);
                 $u->setNome($item['nome']);
                 $u->setEmail($item['email']);
-
-                $array[] = $u;
             }
         }
+
         return $array;
     }
     public function findById($id) {
